400c584947a23f5799baae5fa0b2ea83f4b373f8
pankajkumar <pankajkumar.pvk@gmail.com>
2025-07-18 17:49:20 +0530

    feat:tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1 className="bg-amber-500">Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
